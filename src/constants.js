49674b8af854b0aadfb195e99768cba7301943af
basitkorai <basitalikorai7@gmail.com>
2024-02-16 23:23:36 +0500

    connect db

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+const DB_NAME = "videogames"
+
+
+module.exports = { DB_NAME }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
-const port = 3000
+const connectDB = require('./db/connectDB')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+connectDB()
+// const app = express()
+// const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
